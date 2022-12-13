65ea9f1729adada958a59e27a1003bd0d5d1cb80
Yevgenii <zhekasokol@gmail.com>
2022-12-13 21:00:29 +0200

    Changing in server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-function SendEmail(u,p){
-    console.log("sent Email to" + u)
-}
-SendEmail(process.env.USER_EMAIL)
+//function SendEmail(u,p){
+ //   console.log("sent Email to" + u)
+//}
+//SendEmail(process.env.USER_EMAIL)
 const http=require('http');
 const app=require('./app');
-const port=5000;
+const port=process.env.PORT;
 const server=http.createServer(app);
 server.listen(port,()=>{console.log("server started")});
