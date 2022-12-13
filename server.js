80d6a2b17ad0bcbb25e0b9cfee277e7f8a4338c0
Yevgenii <zhekasokol@gmail.com>
2022-12-13 20:15:46 +0200

    Env Addition and a few workarounds

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+function SendEmail(u,p){
+    console.log("sent Email to" + u)
+}
+SendEmail(process.env.USER_EMAIL)
 const http=require('http');
 const app=require('./app');
 const port=5000;
