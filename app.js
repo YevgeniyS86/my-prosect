50096b9a47efa9365d896d43ab5f382959b1fc4b
Yevgenii <zhekasokol@gmail.com>
2022-12-19 22:36:05 +0200

    Last commit

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express=require('express');
 const app=express();
+const morgan=require('morgan');
 const PrRout= require('./API/V1/routes/product');
+app.use(morgan('dev'));
+app.use(function(req,res,next)
+{
+   const myip = req.socket.remoteAddress;
+console.log("Connection from Ip" + req.socket.remoteAddress);
+next();
+});
+
+
 app.use("/product", PrRout);
+
 app.get("/",(req,res)=>{
     res.status(200).json({"Message":" ecom-web-3"});
        });
